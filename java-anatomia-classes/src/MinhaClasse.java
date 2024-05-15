c750750daf266828c8247940aec90a8cd6aa9965
RomuuloGoncalves <romulogoncalves13@gmail.com>
2024-05-15 16:12:11 -0300

    tipos de variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,3 +1,15 @@
 public class MinhaClasse {
-  
+
+  public static void main(String[] args) {
+    System.out.print("Olá mundo \n");
+
+    String primeiroNome = "Rômulo";
+    String segundoNome = "Gonçalves";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.print(nomeCompleto);
+  }
+
+  public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
+  }
 }
---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,13 @@
+public class TipoVariaveis {
+    public static void main(String[] args) throws Exception {
+       double salarioMinimo = 2500.00;
+       int ano = 2024;
+       float pi = 3.14F;
+       String texto = "Olá mundo";
+
+       //Constantes são identificadas usando o FINAL
+
+       final String NOME = "Rômulo";
+       final double VALOR_PI = 3.14;
+    }
+}
